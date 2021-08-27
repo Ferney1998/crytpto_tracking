120b81ac6f2f275f52d0babf4b418f69fdf083d4
ferney <wcubides@unab.edu.co>
2021-08-27 00:10:50 -0500

    first commit

---
+++ positions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Test(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.URLField()
+    symbol = models.CharField(max_length=30)
+    price = models.CharField(max_length=200)
+    rank = models.CharField(max_length=10)
+    market_cap = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
